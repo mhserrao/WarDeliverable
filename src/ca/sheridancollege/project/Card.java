85a774b9b1b5f3b5acb293d46932a070334b7aaf
maizielserrao <mserr059@uottawa.ca>
2020-03-21 15:03:27 -0400

    Altering spacing

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -7,60 +7,64 @@ package ca.sheridancollege.project;
 
 /**
  * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
+ *
  * @author dancye, 2018
  */
-public abstract class Card 
-{
+public abstract class Card {
+
     public enum Suit {
-    HEARTS (4), 
-    DIAMONDS (3), 
-    SPADES (2), 
-    CLUBS (1);
-    
-    private final int SHAPE;
-    
-    Suit (int shape){
-        this.SHAPE = shape;
-    }
-    
-    public int getShape () {
-        return SHAPE;
-    }
-    }
-    public enum Rank {TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(11),
-    QUEEN(12),
-    KING(13),
-    ACE(14);
+        HEARTS(4),
+        DIAMONDS(3),
+        SPADES(2),
+        CLUBS(1);
 
-    private final int CARDNUMBER;
+        private final int SHAPE;
 
-    Rank(int i) {
-        this.CARDNUMBER = i;
+        Suit(int shape) {
+            this.SHAPE = shape;
+        }
+
+        public int getShape() {
+            return SHAPE;
+        }
     }
-    
-    public int getCardNumber() {
-        return CARDNUMBER;
+
+    public enum Rank {
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(11),
+        QUEEN(12),
+        KING(13),
+        ACE(14);
+
+        private final int CARDNUMBER;
+
+        Rank(int i) {
+            this.CARDNUMBER = i;
+        }
+
+        public int getCardNumber() {
+            return CARDNUMBER;
+        }
     }
-}
     //default modifier for child classes
-    
+
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a
+     * regular playing card etc.
      */
-    
     @Override
     public abstract String toString();
-    
+
 }
